623d7a7756e957130d95f147091c4a671a37b461
Carlos Sousa <kkosmico@gmail.com>
2026-08-15 21:40:06 +0100

    migrations files

---
+++ rest-with-spring-boot-and-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,13 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`)
+VALUES
+(4,'Rua das Pretas','Carlos Campos','Male','Sousa'),
+(5,'Rua das Tretas','Jose','Female','Sousa'),
+(7,'Rua das Francas','Julio Campos','Male','Sousa'),
+(8,'Rua das Amarelas','Carlos Campos','Male','Sousa'),
+(9,'Rua das Outras','Ricardo Campos','Male','SousaCom data'),
+(10,'Rua das Muitsa','Carlos Limao','Male','SousaCom data'),
+(11,'Rua das Haha','Carlos Silva','Male','SousaCom data'),
+(12,'Rua das Jose','Carla Campos','Female','SousaCom data'),
+(13,'Rua das Alvaro','Carlos Campos','Male','SousaCom data'),
+(14,'Rua das Rui','Luis','Male','SousaCom data'),
+(15,'Rua das Pretas','Luisa','Male','SousaCom data');
