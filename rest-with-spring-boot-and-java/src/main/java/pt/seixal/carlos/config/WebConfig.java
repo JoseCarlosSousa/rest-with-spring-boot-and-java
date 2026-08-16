a77fc786bf3e87ae18acbe9a73bc82a57cd5612f
Carlos Sousa <kkosmico@gmail.com>
2026-08-16 10:55:04 +0100

    via header accept

--- rest-with-spring-boot-and-java/src/main/java/pt/seixal/carlos/config/WebConfig.java
+++ rest-with-spring-boot-and-java/src/main/java/pt/seixal/carlos/config/WebConfig.java
@@ -11,9 +11,22 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
+        // via PATH EXTENSION http://localhost:8080/api/person/v1/2.json Deprecated
+
+        // via QUERY PARAM http://localhost:8080/api/person/v1/2?mediaType=xml
+        /*
         configurer.favorParameter(true)
                 .parameterName("mediaType")
                 .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+         */
+
+        // via HEADER ACCEPT http://localhost:8080/api/person/v1/2
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
