f304ce87e1e705d90c4418504499e1ed9174574c
Carlos Sousa <kkosmico@gmail.com>
2026-08-19 00:52:23 +0100

    remover consumes

--- rest-with-spring-boot-and-java/src/main/java/pt/seixal/carlos/controllers/docs/BookControllerDocs.java
+++ rest-with-spring-boot-and-java/src/main/java/pt/seixal/carlos/controllers/docs/BookControllerDocs.java
@@ -37,10 +37,6 @@ public interface BookControllerDocs {
     List<BookDTO> findAll();
 
     @GetMapping(value = "/{id}",
-            consumes = {
-                    MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE,
-                    MediaType.APPLICATION_YAML_VALUE},
             produces = {
                     MediaType.APPLICATION_JSON_VALUE,
                     MediaType.APPLICATION_XML_VALUE,
