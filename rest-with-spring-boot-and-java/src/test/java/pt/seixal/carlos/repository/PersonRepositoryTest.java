aed62361e832b5b99b9c7ddae599594e64913252
Carlos Sousa <kkosmico@gmail.com>
2026-09-20 13:28:16 +0100

    apontar

--- rest-with-spring-boot-and-java/src/test/java/pt/seixal/carlos/repository/PersonRepositoryTest.java
+++ rest-with-spring-boot-and-java/src/test/java/pt/seixal/carlos/repository/PersonRepositoryTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import pt.seixal.carlos.integrationtests.testcontainers.AbstractIntegrationTest;
@@ -22,6 +23,7 @@ import pt.seixal.carlos.model.Person;
 
 @ExtendWith(SpringExtension.class)
 //@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@EnableJpaRepositories(basePackages = "pt.seixal.carlos.repository")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PersonRepositoryTest extends AbstractIntegrationTest {
 
