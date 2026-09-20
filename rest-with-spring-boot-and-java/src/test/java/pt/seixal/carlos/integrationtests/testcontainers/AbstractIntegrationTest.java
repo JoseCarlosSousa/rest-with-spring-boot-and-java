9c411a0df941b6a6e4a7a3bd700202c8ae05f50c
Carlos Sousa <kkosmico@gmail.com>
2026-09-20 11:21:06 +0100

    tudo aponta para a bd do docker

--- rest-with-spring-boot-and-java/src/test/java/pt/seixal/carlos/integrationtests/testcontainers/AbstractIntegrationTest.java
+++ rest-with-spring-boot-and-java/src/test/java/pt/seixal/carlos/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -182,7 +182,7 @@ public abstract class AbstractIntegrationTest {
 
 	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-		static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.1.0");
+		static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:9.1.0");
 
 		private static void startContainers() {
 			Startables.deepStart(Stream.of(mysql)).join();
